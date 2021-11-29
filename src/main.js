bd603e61b500b4fd51a70622a863f892c0694b32
Anna <anna.kulakova1991@gmail.com>
2021-11-29 12:14:49 +0300

    Время разбивать камни_2

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import {createSiteMenuTemplate, createFilterTemplate} from './view/site-menu-view.js';
-import {createStatTemplate} from './view/statistic-view.js';
-import {createCardFilmTemplate} from './view/card-film-view.js';
+import {createFilmListTemplate} from './view/film-list-view.js';
 import {createUsersRankTemplate} from './view/users-rank-view.js';
 import {createBtnShowMore} from './view/btn-show-more-view.js';
-// eslint-disable-next-line no-unused-vars
-import {createPopupTemplate} from './view/popup-view.js';
 
-const FILMS_COUNT = 5;
+
 const siteMainElement = document.querySelector('.main');
 
 
@@ -22,14 +19,13 @@ const renderTemplate = (container, template, place) => {
 
 renderTemplate(siteMainElement, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(siteMainElement, createFilterTemplate(), RenderPosition.BEFOREEND);
-renderTemplate(siteMainElement, createStatTemplate(), RenderPosition.BEFOREEND);
+
 
 const header = document.querySelector('.header');
 renderTemplate(header, createUsersRankTemplate(), RenderPosition.BEFOREEND);
 
-for (let i = 0; i < FILMS_COUNT; i++){
-  renderTemplate(siteMainElement, createCardFilmTemplate(), RenderPosition.BEFOREEND);
-}
+renderTemplate(siteMainElement, createFilmListTemplate(), RenderPosition.BEFOREEND);
+
 renderTemplate(siteMainElement, createBtnShowMore(), RenderPosition.BEFOREEND);
-//renderTemplate(siteMainElement, createPopupTemplate(), RenderPosition.BEFOREEND);
+
 
